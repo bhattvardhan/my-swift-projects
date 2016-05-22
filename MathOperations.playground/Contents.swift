e32f2d161d0796d5547a0147b28e5f547710ebaf
Vardhan Bhatt <vardhanvbhatt@gmail.com>
2016-05-22 13:51:11 +0530

    Math Operation assignment

---
+++ MathOperations.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge = 15
+
+var myExperience = 5
+
+var mySum = myAge + myExperience
+
+var multiplicationValue = 15 * 3
+
+var differenceValue = myAge - myExperience
+
+var divisionResult = myAge / myExperience
+
+var one = 1, two = 2, three = 3
+
+let val1 = 1, val2 = 2, val3 = 3
+
+var myDouble : Double
+
+var myInt : Int = 423
+
+var salut : String = "Hello"
+
+var salute : String = "World"
+
+var greeting = salut + " " + salute
